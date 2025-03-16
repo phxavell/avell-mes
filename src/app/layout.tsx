c29f512350b71954bc610a096c576cd6b8cb7644
paulosena <paulosenamne@gmail.com>
2025-03-15 23:46:07 -0400

    commit inicial, subindo sidebar( mobile + PC)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils"; // adcionado biblioteca Shadcnui
+import { Sidebar } from "@/components/sidebar/index";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          `${geistSans.variable} ${geistMono.variable} antialiased`
+        )}>
+        <Sidebar />
         {children}
       </body>
     </html>
   );
 }
+
